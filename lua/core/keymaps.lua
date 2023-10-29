872cf41955d6df762d7542587ef7c5ab1c6962b4
侯昆 <houkun@houkundeMBP.lan>
2023-10-29 13:09:56 +0800

    add keymaps.lua

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader=' '
+
+local map = vim.keymap.set
+
+map('i','jk','<esc>')
+map('i','kj','<esc>')
+map('n','<leader>w','<cmd>w<cr>')
+map('n','<leader>q','<cmd>q<cr>')
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,3 +2,9 @@ local opt=vim.opt
 opt.nu=true
 opt.rnu=true
 opt.ignorecase=true
+opt.smartcase=true
+
+
+-- tab
+opt.tabstop=4
+opt.shiftwidth=4
